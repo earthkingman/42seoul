498813c7ea1e297772fc7adc2a82cf120142da0a
ji-park <qkrskdjf@naver.com>
2020-12-29 23:28:34 +0900

    마이너스 노플래그

--- ft_printf/ft_make_nd.c
+++ ft_printf/ft_make_nd.c
@@ -1,6 +1,6 @@
 #include "ft_printf.h"
 
-void    n_noflag(t_format *t_node, char *str)
+void n_noflag(t_format *t_node, char *str)
 {
     int i;
     int j;
@@ -13,11 +13,11 @@ void    n_noflag(t_format *t_node, char *str)
         str++;
         size = t_node->size;
         if (t_node->size < t_node->prec)
-            size = t_node->prec;
+            size = t_node->prec + 1;
         while (t_node->wid > size + i)
             t_node->result[i++] = ' ';
         t_node->result[i++] = '-';
-        while (t_node->size + j - 1< t_node->prec)
+        while (t_node->size + j - 1 < t_node->prec)
         {
             t_node->result[i++] = '0';
             j++;
@@ -27,18 +27,17 @@ void    n_noflag(t_format *t_node, char *str)
     }
 }
 
-
-void    n_flag_minus(t_format *t_node, char *str)
+void n_flag_minus(t_format *t_node, char *str)
 {
     int i;
 
     i = 0;
     if (str != NULL)
-    { 
+    {
         str++;
         t_node->result[i++] = '-';
-        while (t_node->prec > t_node->size + i - 2 )
-            t_node->result[i++] = '0';       
+        while (t_node->prec > t_node->size + i - 2)
+            t_node->result[i++] = '0';
         while (*str != 0)
             t_node->result[i++] = *str++;
         while (i < t_node->max_size)
