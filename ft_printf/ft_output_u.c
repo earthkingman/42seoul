31e935f8ad1f78b760302c1f576776108565497b
ji-park <qkrskdjf@naver.com>
2020-12-30 07:43:29 +0900

    fix u

--- ft_printf/ft_output_u.c
+++ ft_printf/ft_output_u.c
@@ -6,6 +6,7 @@ void    ft_output_u(t_format *t_node)
     
     u = (unsigned  int)va_arg(t_node->ap, unsigned int);
     u_to_str(t_node, u);
+    exception_null(t_node, u);
     judge_max(t_node);
     if ((t_node->result = (char *)malloc(sizeof(char) * (t_node->max_size + 1))) == 0)
         return ;
@@ -13,6 +14,8 @@ void    ft_output_u(t_format *t_node)
     t_node->result[t_node->max_size] = 0;
     if (t_node->flag[0] == 1)
         flag_minus(t_node, t_node->num);
+    else if (t_node->flag[1] == 1)
+        flag_zero(t_node, t_node->num);
     else
-        noflag(t_node, t_node->num); 
+        noflag(t_node, t_node->num);
 }
