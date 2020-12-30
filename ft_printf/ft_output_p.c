3f2dbf06aab8d7f5afe255331440e7471b346c12
ji-park <qkrskdjf@naver.com>
2020-12-31 00:51:26 +0900

    p 백업

--- ft_printf/ft_output_p.c
+++ ft_printf/ft_output_p.c
@@ -8,12 +8,10 @@ void    p_flag_minus(t_format *t_node, char *str)
     i = 0;
     j = 0;
     if (str != NULL)
-    {   
-        t_node->result[i++] = '0';
-        t_node->result[i++] = 'x';  
+    {
         while (*str != 0)
             t_node->result[i++] = *str++;
-        while (t_node->wid > t_node->size + 2 + j)
+        while (t_node->wid > t_node->size + j)
         {
             t_node->result[i++] = ' ';
             j++;
@@ -36,8 +34,6 @@ void    p_noflag(t_format *t_node, char *str)
 			size = t_node->prec;
 		while (t_node->wid > size + i)
 			t_node->result[i++] = ' ';
-        t_node->result[i++] = '0';
-        t_node->result[i++] = 'x'; 
 		while (t_node->size + j < t_node->prec)
 		{
 			t_node->result[i++] = '0';
@@ -55,9 +51,9 @@ void    ft_output_p(t_format *t_node)
     p = (unsigned long int)va_arg(t_node->ap, void *);
     h_to_str(p, t_node);
     judge_max(t_node);
-    if ((t_node->result = (char *)malloc(sizeof(char) * (t_node->max_size + 1))) == 0)
+    if ((t_node->result = (char *)malloc(sizeof(char) * (t_node->max_size + 3))) == 0)
         return ;
-    pf_bzero(t_node->result,t_node->max_size + 1);
+    pf_bzero(t_node->result,t_node->max_size + 3);
     if (t_node->flag[0] == 1)
         p_flag_minus(t_node, t_node->num);
     else
