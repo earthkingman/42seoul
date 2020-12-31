a282a0fefa1d42dac1eb39f93bfc428537faec7f
ji-park <qkrskdjf@naver.com>
2021-01-01 00:05:41 +0900

    c 플래그 새로만듬

--- ft_printf/ft_output_c.c
+++ ft_printf/ft_output_c.c
@@ -15,9 +15,9 @@ void    ft_output_c(t_format *t_node)
         return ;
     pf_bzero(t_node->result,t_node->max_size + 1);
     if (t_node->flag[0] == 1)
-        s_flag_minus(t_node, t_node->num);
+        c_flag_minus(t_node, t_node->num);
     else if (t_node->flag[1] == 1)
-        s_flag_zero(t_node, t_node->num);
+        c_flag_zero(t_node, t_node->num);
     else
-        s_noflag(t_node, t_node->num);
+        c_noflag(t_node, t_node->num);
 }
