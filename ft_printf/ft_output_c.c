ba661cc62dddd1733c2a82e9d0dcdda8230f2ed3
ji-park <qkrskdjf@naver.com>
2021-01-01 00:08:04 +0900

    c fix

--- ft_printf/ft_output_c.c
+++ ft_printf/ft_output_c.c
@@ -5,19 +5,17 @@ void    ft_output_c(t_format *t_node)
     unsigned char c;
 
     c = (unsigned char)va_arg(t_node->ap, int);
-    t_node->num[0] = c;
-    t_node->num[1] = 0;
     if (c == 0)
-        t_node->num[0] = '\0';
+        c = '\0';
     t_node->size = 1;
     judge_max(t_node);
     if ((t_node->result = (char *)malloc(sizeof(char) * (t_node->max_size + 1))) == 0)
         return ;
     pf_bzero(t_node->result,t_node->max_size + 1);
     if (t_node->flag[0] == 1)
-        c_flag_minus(t_node, t_node->num);
+        c_flag_minus(t_node, c);
     else if (t_node->flag[1] == 1)
-        c_flag_zero(t_node, t_node->num);
+        c_flag_zero(t_node, c);
     else
-        c_noflag(t_node, t_node->num);
+        c_noflag(t_node, c);
 }
