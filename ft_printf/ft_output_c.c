c94e67211f6d0eafe91273e85d79571f13e70396
ji-park <qkrskdjf@naver.com>
2020-12-31 18:40:15 +0900

    help

--- ft_printf/ft_output_c.c
+++ ft_printf/ft_output_c.c
@@ -2,23 +2,22 @@
 
 void    ft_output_c(t_format *t_node)
 {   
-    char str[2];
     unsigned char c;
 
     c = (unsigned char)va_arg(t_node->ap, int);
-    str[0] = c;
-    str[1] = 0;
+    t_node->num[0] = c;
+    t_node->num[1] = 0;
     if (c == 0)
-        str[0] = '\0';
+        t_node->num[0] = '\0';
     t_node->size = 1;
     judge_max(t_node);
     if ((t_node->result = (char *)malloc(sizeof(char) * (t_node->max_size + 1))) == 0)
         return ;
     pf_bzero(t_node->result,t_node->max_size + 1);
     if (t_node->flag[0] == 1)
-        s_flag_minus(t_node, str);
+        s_flag_minus(t_node, t_node->num);
     else if (t_node->flag[1] == 1)
-        s_flag_zero(t_node, str);
+        s_flag_zero(t_node, t_node->num);
     else
-        s_noflag(t_node, str);
+        s_noflag(t_node, t_node->num);
 }
--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -99,7 +99,7 @@ void    ft_putstr(t_format *t_node)
     int i;
 
     i = 0;
-    if (t_node->spec == 'c')
+    if (t_node->spec == 'c' && t_node->num == '\0')
     {
        write(1, &t_node->result, t_node->max_size); 
        return ;
