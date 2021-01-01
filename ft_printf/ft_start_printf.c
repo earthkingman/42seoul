63f8db3643de054c412e2a687d252b28a493770d
Ji yul Park <ji-park@c4r9s1.42seoul.kr>
2021-01-02 02:05:57 +0900

    norm

--- ft_printf/ft_start_printf.c
+++ ft_printf/ft_start_printf.c
@@ -1,15 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_start_printf.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ji-park <gudor123@nate.com>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/02 01:59:40 by ji-park           #+#    #+#             */
+/*   Updated: 2021/01/02 02:00:59 by ji-park          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
 void	start_printf(t_format *t_node)
 {
 	while (*t_node->str != 0)
-    {
-        if (*t_node->str == '%')
+	{
+		if (*t_node->str == '%')
 		{
 			t_node->str++;
 			check_flag(t_node);
-        	check_wid(t_node);
-        	check_prec(t_node);
+			check_wid(t_node);
+			check_prec(t_node);
 			check_spec(t_node);
 			if (t_node->spec == '0')
 				return ;
@@ -18,13 +30,13 @@ void	start_printf(t_format *t_node)
 			ft_putstr(t_node);
 			init_node(t_node);
 		}
-        else
-        {
-            ft_putchar(*t_node->str);
-            t_node->str++;
+		else
+		{
+			ft_putchar(*t_node->str);
+			t_node->str++;
 			t_node->nums++;
-        }
-    }
+		}
+	}
 }
 
 void	ft_output(t_format *t_node)
@@ -35,19 +47,19 @@ void	ft_output(t_format *t_node)
 		ft_output_s(t_node);
 	else if (t_node->spec == 'd')
 		ft_output_d(t_node);
-	else if(t_node->spec == 'i')
+	else if (t_node->spec == 'i')
 		ft_output_d(t_node);
-	else if(t_node->spec == 'c')
+	else if (t_node->spec == 'c')
 		ft_output_c(t_node);
-	else if(t_node->spec == 'p')
+	else if (t_node->spec == 'p')
 		ft_output_p(t_node);
-	else if(t_node->spec == 'u')
+	else if (t_node->spec == 'u')
 		ft_output_u(t_node);
-	else if(t_node->spec == 'x')
+	else if (t_node->spec == 'x')
 		ft_output_x(t_node);
-	else if(t_node->spec == 'X')
+	else if (t_node->spec == 'X')
 		ft_output_x(t_node);
-	else if(t_node->spec == '%')
+	else if (t_node->spec == '%')
 		ft_output_percent(t_node);
 }
 
--- ft_printf/ft_to_str.c
+++ ft_printf/ft_to_str.c
@@ -1,27 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_to_str.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ji-park <gudor123@nate.com>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/02 02:01:28 by ji-park           #+#    #+#             */
+/*   Updated: 2021/01/02 02:04:55 by ji-park          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
 void	d_to_str(t_format *t_node, int n)
 {
-	int			len;
+	int				len;
 	unsigned int	tmp;
 
 	if (n == 0)
-    {
-        t_node->num[0] = '0';
-        t_node->num[1] = 0;
-        t_node->size = 1;
-        return ;
-    }
+	{
+		t_node->num[0] = '0';
+		t_node->num[1] = 0;
+		t_node->size = 1;
+		return ;
+	}
 	len = check_size(n);
 	if (n < 0)
-    {
+	{
 		tmp = n * -1;
-        len++;
-        t_node->num[0] = '-';
-    }
+		len++;
+		t_node->num[0] = '-';
+	}
 	else
 		tmp = n;
-    t_node->size = len;
+	t_node->size = len;
 	t_node->num[len] = 0;
 	len--;
 	do_itoa(tmp, t_node, len);
@@ -29,104 +41,104 @@ void	d_to_str(t_format *t_node, int n)
 
 void	u_to_str(t_format *t_node, unsigned int n)
 {
-	int			len;
+	int				len;
 	unsigned int	tmp;
 
 	if (n == 0)
-    {
-        t_node->num[0] = '0';
-        t_node->num[1] = 0;
-        t_node->size = 1;
-        return ;
-    }
+	{
+		t_node->num[0] = '0';
+		t_node->num[1] = 0;
+		t_node->size = 1;
+		return ;
+	}
 	len = check_size(n);
 	tmp = n;
-    t_node->size = len;
+	t_node->size = len;
 	t_node->num[len] = 0;
 	len--;
 	do_itoa(tmp, t_node, len);
 }
 
-void     h_to_str(long long addr, t_format *t_node)
+void	h_to_str(long long addr, t_format *t_node)
 {
-    char    g_hex_s[17] = "0123456789abcdef";
-    long long temp;
-    int     len;
-    
-    len = 0;
-    temp = addr;
-    while (addr != 0)
-    {
-        addr /= 16;
-        len++;
-    }
-    len = len == 0 ? 3 : len + 2;
-    t_node->size = len;
-    addr = temp;
-    while (addr != 0)
-    {
-        t_node->num[--len] = g_hex_s[addr % 16];
-        addr /= 16;
-    }
-    t_node->num[0] = '0';
-    t_node->num[1] = 'x';
-    t_node->num[2] = temp == 0 ? '0' : t_node->num[2];
+	char		g_hex_s[17] = "0123456789abcdef";
+	long long	temp;
+    int			len;
+
+	len = 0;
+	temp = addr;
+	while (addr != 0)
+	{
+		addr /= 16;
+		len++;
+	}
+	len = len == 0 ? 3 : len + 2;
+	t_node->size = len;
+	addr = temp;
+	while (addr != 0)
+	{
+		t_node->num[--len] = g_hex_s[addr % 16];
+		addr /= 16;
+	}
+	t_node->num[0] = '0';
+	t_node->num[1] = 'x';
+	t_node->num[2] = temp == 0 ? '0' : t_node->num[2];
 }
 
 void     sx_to_str(t_format *t_node, unsigned int addr)
 {
-    char    g_hex_s[17] = "0123456789abcdef";
-    long long temp;
-    int     len;
-    
-    if (addr == 0)
-    {
-        t_node->num[0] = '0';
-        t_node->size = 1;
-        return ;
-    }
-    len = 0;
-    temp = addr;
-    while (addr != 0)
-    {
-        addr /= 16;
-        len++;
-    }
-    len = len == 0 ? 0 : len;
-    t_node->size = len;
-    addr = temp;
-    while (addr != 0)
-    {
-        t_node->num[--len] = g_hex_s[addr % 16];
-        addr /= 16;
-    }
+	char    g_hex_s[17] = "0123456789abcdef";
+	long long temp;
+	int     len;
+
+	if (addr == 0)
+	{
+		t_node->num[0] = '0';
+		t_node->size = 1;
+		return ;
+	}
+	len = 0;
+	temp = addr;
+	while (addr != 0)
+	{
+		addr /= 16;
+		len++;
+	}
+	len = len == 0 ? 0 : len;
+	t_node->size = len;
+	addr = temp;
+	while (addr != 0)
+	{
+		t_node->num[--len] = g_hex_s[addr % 16];
+		addr /= 16;
+	}
 }
 
-void     bx_to_str(t_format *t_node, unsigned int addr)
+void	bx_to_str(t_format *t_node, unsigned int addr)
 {
-    char    g_hex_s[17] = "0123456789ABCDEF";
-    long long temp;
-    int     len;
-    
-    if (addr == 0)
-    {
-        t_node->num[0] = '0';
-        t_node->size = 1;
-        return ;
-    }
-    len = 0;
-    temp = addr;
-    while (addr != 0)
-    {
-        addr /= 16;
-        len++;
-    }
-    len = len == 0 ? 0 : len;
-    t_node->size = len;
-    addr = temp;
-    while (addr != 0)
-    {
-        t_node->num[--len] = g_hex_s[addr % 16];
-        addr /= 16;
-    }
+	char		g_hex_s[17] = "0123456789ABCDEF";
+	long long	temp;
+	int			len;
+
+	if (addr == 0)
+	{
+		t_node->num[0] = '0';
+		t_node->size = 1;
+		return ;
+	}
+	len = 0;
+	temp = addr;
+	while (addr != 0)
+	{
+		addr /= 16;
+		len++;
+	}
+	len = len == 0 ? 0 : len;
+	t_node->size = len;
+	addr = temp;
+	while (addr != 0)
+	{
+		t_node->num[--len] = g_hex_s[addr % 16];
+		addr /= 16;
+	}
 }
