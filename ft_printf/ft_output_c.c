2cf1e6d0ff0b8c47254992f9bd1f6aa9102c037d
Ji yul Park <ji-park@c4r9s1.42seoul.kr>
2021-01-02 00:29:39 +0900

    norm

--- ft_printf/ft_output_c.c
+++ ft_printf/ft_output_c.c
@@ -1,21 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_output_c.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ji-park <gudor123@nate.com>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/02 00:21:10 by ji-park           #+#    #+#             */
+/*   Updated: 2021/01/02 00:23:47 by ji-park          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-void    ft_output_c(t_format *t_node)
-{   
-    unsigned char c;
+void	ft_output_c(t_format *t_node)
+{
+	unsigned char	c;
+	int				i;
 
-    c = (unsigned char)va_arg(t_node->ap, int);
-    if (c == 0)
-        c = '\0';
-    t_node->size = 1;
-    judge_max(t_node);
-    if ((t_node->result = (char *)malloc(sizeof(char) * (t_node->max_size + 1))) == 0)
-        return ;
-    pf_bzero(t_node->result,t_node->max_size + 1);
-    if (t_node->flag[0] == 1)
-        c_flag_minus(t_node, c);
-    else if (t_node->flag[1] == 1)
-        c_flag_zero(t_node, c);
-    else
-        c_noflag(t_node, c);
+	c = (unsigned char)va_arg(t_node->ap, int);
+	if (c == 0)
+		c = '\0';
+	t_node->size = 1;
+	judge_max(t_node);
+	i = t_node->max_size;
+	if ((t_node->result = (char *)malloc(sizeof(char) * (i + 1))) == 0)
+		return ;
+	pf_bzero(t_node->result, t_node->max_size + 1);
+	if (t_node->flag[0] == 1)
+		c_flag_minus(t_node, c);
+	else if (t_node->flag[1] == 1)
+		c_flag_zero(t_node, c);
+	else
+		c_noflag(t_node, c);
 }
--- ft_printf/ft_output_d.c
+++ ft_printf/ft_output_d.c
@@ -1,48 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_output_d.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ji-park <gudor123@nate.com>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/02 00:26:19 by ji-park           #+#    #+#             */
+/*   Updated: 2021/01/02 00:29:24 by ji-park          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-
-void    ft_output_d(t_format *t_node)
+void	ft_output_d(t_format *t_node)
 {
-    int num;
-    
-    num = va_arg(t_node->ap, int);
-    if (num >= 0)
-        positive_d(t_node, num);
-    else
-        negative_d(t_node, num);
+	int num;
+
+	num = va_arg(t_node->ap, int);
+	if (num >= 0)
+		positive_d(t_node, num);
+	else
+		negative_d(t_node, num);
 }
 
-
-void    positive_d(t_format *t_node, int num)
+void	positive_d(t_format *t_node, int num)
 {
-    d_to_str(t_node, num);
-    exception_null(t_node, num);
-    judge_max(t_node);
-    if ((t_node->result = (char *)malloc(sizeof(char) * (t_node->max_size + 1))) == 0)
-        return ;
-    pf_bzero(t_node->result,t_node->max_size + 1);
-    if (t_node->flag[0] == 1)
-        flag_minus(t_node, t_node->num);
-    else if (t_node->flag[1] == 1)
-        flag_zero(t_node, t_node->num);
-    else
-        noflag(t_node, t_node->num);
+	d_to_str(t_node, num);
+	exception_null(t_node, num);
+	judge_max(t_node);
+	if ((t_node->result = (char *)malloc(sizeof(char) * (t_node->max_size + 1))) == 0)
+		return ;
+	pf_bzero(t_node->result,t_node->max_size + 1);
+	if (t_node->flag[0] == 1)
+		flag_minus(t_node, t_node->num);
+	else if (t_node->flag[1] == 1)
+		flag_zero(t_node, t_node->num);
+	else
+		noflag(t_node, t_node->num);
 }
 
-
-void    negative_d(t_format *t_node, int num)
+void	negative_d(t_format *t_node, int num)
 {
-    d_to_str(t_node, num);
+	d_to_str(t_node, num);
     n_judge_max(t_node);
-    if ((t_node->result = (char *)malloc(sizeof(char) * (t_node->max_size + 1))) == 0)
-        return ;
-    pf_bzero(t_node->result,t_node->max_size + 1);
-    if (t_node->flag[0] == 1)
-        n_flag_minus(t_node, t_node->num);
-    else if (t_node->flag[1] == 1)
-        n_flag_zero(t_node, t_node->num);
-    else
-        n_noflag(t_node, t_node->num);
+	if ((t_node->result = (char *)malloc(sizeof(char) * (t_node->max_size + 1))) == 0)
+		return ;
+	pf_bzero(t_node->result,t_node->max_size + 1);
+	if (t_node->flag[0] == 1)
+		n_flag_minus(t_node, t_node->num);
+	else if (t_node->flag[1] == 1)
+		n_flag_zero(t_node, t_node->num);
+	else
+		n_noflag(t_node, t_node->num);
 }
 
 void   exception_null(t_format *t_node, int num)
