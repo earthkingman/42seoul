c7ed56bd67dadb008fef06e06007a8725cfe1c06
ji-park <qkrskdjf@naver.com>
2021-01-02 01:07:37 +0900

    norm

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -12,6 +12,27 @@
 
 #include "ft_printf.h"
 
+int		ft_printf(const char *format, ...)
+{
+	int	size;
+
+	t_format *t_node;
+	t_node = (t_format*)malloc(sizeof(t_format));
+	va_start(t_node->ap, format);
+	init_firstnode(t_node, (char *)format);
+	start_printf(t_node);
+	if (t_node->spec == '0')
+	{
+		va_end(t_node->ap);
+		free(t_node);
+		return (-1);
+	}
+	va_end(t_node->ap);
+	size = t_node->nums;
+	free(t_node);
+	return (size);
+}
+
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
@@ -38,25 +59,4 @@ void	ft_putstr(t_format *t_node)
 		i++;
 	}
 	t_node->nums += pf_strlen(t_node->result);
-}
-
-int		ft_printf(const char *format, ...)
-{
-	int	size;
-
-	t_format *t_node;
-	t_node = (t_format*)malloc(sizeof(t_format));
-	va_start(t_node->ap, format);
-	init_firstnode(t_node, (char *)format);
-	start_printf(t_node);
-	if (t_node->spec == '0')
-	{
-		va_end(t_node->ap);
-		free(t_node);
-		return (-1);
-	}
-	va_end(t_node->ap);
-	size = t_node->nums;
-	free(t_node);
-	return (size);
 }
