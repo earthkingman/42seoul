fb1f65d2f8603e8669ee7a031f0c18d3c440c406
Ji yul Park <ji-park@c4r9s1.42seoul.kr>
2021-01-02 01:05:46 +0900

    printf norm

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ji-park <gudor123@nate.com>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/02 01:01:09 by ji-park           #+#    #+#             */
+/*   Updated: 2021/01/02 01:04:40 by ji-park          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
 void	ft_putchar(char c)
@@ -5,46 +17,46 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void    ft_putstr(t_format *t_node)
+void	ft_putstr(t_format *t_node)
 {
-    int i;
+	int i;
 
-    i = 0;
-    if (t_node->spec == 'c' && t_node->num[0] == '\0')
-    {
-         while(i < t_node->max_size)
-        {
-            write(1, &t_node->result[i], 1);
-            i++;
-        }
-        t_node->nums += t_node->max_size;
-        return ;
-    }
-    while(t_node->result[i] != 0)
-    {
-        write(1, &t_node->result[i], 1);
-        i++;
-    }
-    t_node->nums += pf_strlen(t_node->result);
+	i = 0;
+	if (t_node->spec == 'c' && t_node->num[0] == '\0')
+	{
+		while (i < t_node->max_size)
+		{
+			write(1, &t_node->result[i], 1);
+			i++;
+		}
+		t_node->nums += t_node->max_size;
+		return ;
+	}
+	while (t_node->result[i] != 0)
+	{
+		write(1, &t_node->result[i], 1);
+		i++;
+	}
+	t_node->nums += pf_strlen(t_node->result);
 }
 
-int ft_printf(const char *format, ...)
+int		ft_printf(const char *format, ...)
 {
-	int size;
+	int	size;
 
-    t_format *t_node;
-    t_node = (t_format*)malloc(sizeof(t_format));
-    va_start(t_node->ap,format);
-    init_firstnode(t_node,(char *)format);
-    start_printf(t_node);
-    if (t_node->spec == '0')
-    {
-        va_end(t_node->ap);
-        free(t_node);
-        return -1;
-    }
-    va_end(t_node->ap);
+	t_format *t_node;
+	t_node = (t_format*)malloc(sizeof(t_format));
+	va_start(t_node->ap, format);
+	init_firstnode(t_node, (char *)format);
+	start_printf(t_node);
+	if (t_node->spec == '0')
+	{
+		va_end(t_node->ap);
+		free(t_node);
+		return (-1);
+	}
+	va_end(t_node->ap);
 	size = t_node->nums;
-    free(t_node);
-	return(size);
+	free(t_node);
+	return (size);
 }
