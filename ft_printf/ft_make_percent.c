b3a1a431ab2a81f3d4adc42eeccc29cd00f295ad
ji-park <qkrskdjf@naver.com>
2021-01-03 16:23:42 +0900

    percent

---
+++ ft_printf/ft_make_percent.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_make_s.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ji-park <gudor123@nate.com>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/02 00:17:10 by ji-park           #+#    #+#             */
+/*   Updated: 2021/01/02 00:17:23 by ji-park          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	percent_noflag(t_format *t_node, char *str)
+{
+	int i;
+	int size;
+
+	i = 0;
+	size = t_node->size;
+	if (t_node->flag[2] == 1)
+	{
+		if (t_node->prec < t_node->size)
+			size = t_node->prec;
+	}
+	if (str != NULL)
+	{
+		while (size + i < t_node->wid)
+		{
+			t_node->result[i++] = ' ';
+		}
+		while (*str != 0 && size > 0)
+		{
+			t_node->result[i++] = *str++;
+			size--;
+		}
+	}
+}
+
+void	percent_flag_minus(t_format *t_node, char *str)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	if (t_node->flag[2] == 1)
+	{
+		while (j < t_node->size && i <= t_node->prec)
+			t_node->result[i++] = str[j++];
+	}
+	else
+	{
+		while (j < t_node->size)
+			t_node->result[i++] = str[j++];
+	}
+	while (i < t_node->wid)
+		t_node->result[i++] = ' ';
+}
+
+void	percent_flag_zero(t_format *t_node, char *str)
+{
+	int i;
+	int size;
+
+	i = 0;
+	size = t_node->size;
+	if (t_node->flag[2] == 1)
+	{
+		if (t_node->prec < t_node->size)
+			size = t_node->prec;
+	}
+	if (str != NULL)
+	{
+		while (size + i < t_node->wid)
+		{
+			t_node->result[i++] = '0';
+		}
+		while (*str != 0 && size > 0)
+		{
+			t_node->result[i++] = *str++;
+			size--;
+		}
+	}
+}
--- ft_printf/ft_output_percent.c
+++ ft_printf/ft_output_percent.c
@@ -25,14 +25,14 @@ void	ft_output_percent(t_format *t_node)
 	pf_bzero(t_node->result, t_node->max_size + 1);
 	if (t_node->flag[0] == 1)
 	{
-		s_flag_minus(t_node, t_node->num);
+		percent_flag_minus(t_node, t_node->num);
 	}
 	else if (t_node->flag[1] == 1)
 	{
-		s_flag_zero(t_node, t_node->num);
+		percent_flag_zero(t_node, t_node->num);
 	}
 	else
 	{
-		s_noflag(t_node, t_node->num);
+		percent_noflag(t_node, t_node->num);
 	}
 }
