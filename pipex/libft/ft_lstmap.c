576e08e5dd08eb1af45065f7c2d2c32acd7fecd3
ji-park <qkrskdjf@naver.com>
2021-10-25 16:27:23 +0900

    norm

--- pipex/libft/ft_lstmap.c
+++ pipex/libft/ft_lstmap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ji-park <gudor123@nate.com>                +#+  +:+       +#+        */
+/*   By: ji-park <ji-park@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 22:34:59 by ji-park           #+#    #+#             */
-/*   Updated: 2020/10/19 22:35:01 by ji-park          ###   ########.fr       */
+/*   Updated: 2021/10/25 16:27:14 by ji-park          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,26 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*newlst;
-	t_list	*curr;
-	t_list	*temp;
+	t_list	*head;
+	t_list	*new;
 
-	if (lst == 0 || f == 0)
-		return (0);
-	if ((newlst = ft_lstnew(f(lst->content))) == 0)
-		return (0);
-	curr = newlst;
+	if (!lst || !f)
+		return (NULL);
+	head = ft_lstnew(f(lst->content));
+	if (!head)
+		return (NULL);
+	new = head;
 	lst = lst->next;
 	while (lst)
 	{
-		if ((temp = ft_lstnew(f(lst->content))) == 0)
+		new->next = ft_lstnew(f(lst->content));
+		if (!new->next)
 		{
-			ft_lstclear(&newlst, del);
-			return (0);
+			ft_lstclear(&head, del);
+			return (NULL);
 		}
-		curr->next = temp;
-		curr = temp;
 		lst = lst->next;
+		new = new->next;
 	}
-	return (newlst);
-}
+	return (head);
+	}
--- pipex/libft/ft_strjoin.c
+++ pipex/libft/ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ji-park <gudor123@nate.com>                +#+  +:+       +#+        */
+/*   By: ji-park <ji-park@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/08 18:38:30 by ji-park           #+#    #+#             */
-/*   Updated: 2020/10/10 12:26:01 by ji-park          ###   ########.fr       */
+/*   Updated: 2021/10/25 16:26:25 by ji-park          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,29 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*str;
-	int		s1_len;
-	int		s2_len;
-	int		i;
-	int		j;
+	char	*s3;
+	int		l1;
+	int		l2;
 
-	i = 0;
-	j = 0;
-	if (s1 == 0 || s2 == 0)
-		return (0);
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	if ((str = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1))) == 0)
-		return (0);
-	while (i < s1_len)
+	if (!s1)
+		return (NULL);
+	l1 = ft_strlen(s1);
+	l2 = ft_strlen(s2);
+	s3 = (char *)malloc(sizeof(char) * (l1 + l2 + 1));
+	if (!s3)
+		return (NULL);
+	l1 = 0;
+	while (s1[l1])
 	{
-		str[i] = s1[i];
-		i++;
+		s3[l1] = s1[l1];
+		l1++;
 	}
-	while (j != s2_len)
-		str[i++] = s2[j++];
-	str[i] = 0;
-	return (str);
-}
+	l2 = 0;
+	while (s2[l2])
+	{
+		s3[l1 + l2] = s2[l2];
+		l2++;
+	}
+	s3[l1 + l2] = 0;
+	return (s3);
+	}
