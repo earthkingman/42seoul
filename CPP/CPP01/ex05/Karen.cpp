ea87384b17654e66e55db5910d0291ce714b2fd1
ji-park <qkrskdjf@naver.com>
2021-12-16 00:39:57 +0900

    CPP 01 수정

--- CPP/CPP01/ex05/Karen.cpp
+++ CPP/CPP01/ex05/Karen.cpp
@@ -11,49 +11,33 @@ Karen::Karen(void)
 void Karen::complain(std::string level)
 {
     std::string table[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-    std::string *p = std::find(table, table + 4, level);
-    int idx = p - table;
 
-    switch (idx)
+    for (int i = 0; i < 4; ++i)
     {
-    case 0:
-        (this->*f[0])();
-        break;
-    case 1:
-        (this->*f[1])();
-        break;
-    case 2:
-        (this->*f[2])();
-        break;
-    case 3:
-        (this->*f[3])();
-        break;
-    default:
-        std::cout << "[ Probably complaining about insignificant problems ]\n";
-        break;
+        if (level == table[i])
+        {
+            (this->*f[i])();
+            return;
+        }
     }
 }
 
 void Karen::debug(void)
 {
-    std::cout << "[ DEBUG ]\n"
-              << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!\n";
+    std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!\n";
 }
 
 void Karen::info(void)
 {
-    std::cout << "[ INFO ]\n"
-              << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!\n";
+    std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!\n";
 }
 
 void Karen::warning(void)
 {
-    std::cout << "[ WARNING ]\n"
-              << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month.\n";
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month.\n";
 }
 
 void Karen::error(void)
 {
-    std::cout << "[ ERROR ]\n"
-              << "This is unacceptable, I want to speak to the manager now.\n";
+    std::cout << "This is unacceptable, I want to speak to the manager now.\n";
 }
